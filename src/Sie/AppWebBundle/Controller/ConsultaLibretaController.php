7aca9cb4c91488709771d44eec2f45ec1e5e3bd8
krlos <pckrlos@gmail.com>
2018-01-09 12:07:58 -0400

    fixed the consulta libreta option, now we can see the last inscription - siged

--- src/Sie/AppWebBundle/Controller/ConsultaLibretaController.php
+++ src/Sie/AppWebBundle/Controller/ConsultaLibretaController.php
@@ -81,16 +81,43 @@ class ConsultaLibretaController extends Controller {
 
         //$form['codigoRude'] = ($sesion->get('rude')) ? $sesion->get('rude') : $estudiante[0]['codigoRude'];
         //$form['codigoRude'] = $estudiante[0]['codigoRude'];
-        $form['gestion'] = isset($form['gestion']) ? $form['gestion'] : $this->session->get('currentyear');
+        // dump($form);die;
+        // $form['gestion'] = isset($form['gestion']) ? $form['gestion'] : $this->session->get('currentyear');
 
-        //get the students inscription
-        $objInscriptionStudent = $em->getRepository('SieAppWebBundle:Estudiante')->getStudentInscriptionData($objStudent[0]['id'], $form['gestion']);
+// dump(($form));die;
+        if(isset($form['gestion'])){
+          $objInscriptionStudent = $em->getRepository('SieAppWebBundle:Estudiante')->getStudentInscriptionData($objStudent[0]['id'], $form['gestion']);
+          if (!($objInscriptionStudent)) {
+              //return al misma opcion de busqueda con el mensaje indicado q no existe el estdiante
+              $session->getFlashBag()->add('notice', 'Estudiante no presenta Historial con estado EFECTIVO/PROMOVIDO en la gestión '.$form['gestion']);
+              return $this->redirect($this->generateUrl('consultalibreta'));
+          }
+          $gestionUse = $form['gestion'];
+        }else {
+          //this for the new to look for the last inscription
+          //get the last incription
+          $inscriptions = $this->getCurrentInscriptionsStudent($objStudent[0]['codigoRude']);
+          reset($inscriptions);
+          $sw = true;
+          $arrAllowMatriculas = array(4,5,56,57,58);
+          //look for the current inscription on this year
+          while($sw &&  ($inscription = current($inscriptions))){
 
-        if (!($objInscriptionStudent)) {
-            //return al misma opcion de busqueda con el mensaje indicado q no existe el estdiante
-            $session->getFlashBag()->add('notice', 'Estudiante no presenta Historial con estado EFECTIVO/PROMOVIDO en la gestión '.$this->session->get('currentyear'));
-            return $this->redirect($this->generateUrl('consultalibreta'));
+              if(in_array($inscription['estadoMatriculaId'], $arrAllowMatriculas)){
+                $objInscriptionStudent = $em->getRepository('SieAppWebBundle:Estudiante')->getStudentInscriptionData($objStudent[0]['id'], $inscription['gestion']);
+                $gestionUse = $inscription['gestion'];
+                $sw=false;
+              }
+            next($inscriptions);
+          }
+
+          if (($sw)) {
+              //return al misma opcion de busqueda con el mensaje indicado q no existe el estdiante
+              $session->getFlashBag()->add('notice', 'Estudiante no presenta Historial con estado EFECTIVO/PROMOVIDO en la gestión '.$this->session->get('currentyear'));
+              return $this->redirect($this->generateUrl('consultalibreta'));
+          }
         }
+
         $objNota = $em->getRepository('SieAppWebBundle:EstudianteInscripcion')->getNotasStudentNew( $objInscriptionStudent[0]['inscripcionid'],$objStudent[0]['id'], $objInscriptionStudent[0]['nivel'], $objInscriptionStudent[0]['grado'], $objInscriptionStudent[0]['paralelo'], $objInscriptionStudent[0]['turno'], $objInscriptionStudent[0]['gestion'], $objInscriptionStudent[0]['sie']);
 
         $aNota = array();
@@ -115,13 +142,50 @@ class ConsultaLibretaController extends Controller {
                 ->getForm();
 
         return $this->render('SieAppWebBundle:ConsultaLibreta:resultlibreta.html.twig', array(
-                    'gestion' => $form['gestion'],
+                    'gestion' => $gestionUse,
                     'bimestres' => $aBim,
                     //'datastudent' => $datastudent,
                     'notastudent' => $aNota,
                     'notacualitativostudent' => $aNota,
                     "form" => $formsearch->createView(),
         ));
+    }
+
+    /**
+     * get the stutdents inscription - the record
+     * @param type $id
+     * @return \Sie\AppWebBundle\Controller\Exception
+     */
+    private function getCurrentInscriptionsStudent($id) {
+//$session = new Session();
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SieAppWebBundle:Estudiante');
+        $query = $entity->createQueryBuilder('e')
+                ->select('n.nivel as nivel', 'g.grado as grado', 'p.paralelo as paralelo', 't.turno as turno', 'em.estadomatricula as estadoMatricula', 'IDENTITY(iec.nivelTipo) as nivelId',
+                 'IDENTITY(iec.gestionTipo) as gestion', 'IDENTITY(iec.gradoTipo) as gradoId', 'IDENTITY(iec.turnoTipo) as turnoId', 'IDENTITY(ei.estadomatriculaTipo) as estadoMatriculaId',
+                 'IDENTITY(iec.paraleloTipo) as paraleloId', 'ei.fechaInscripcion', 'i.id as sie', 'i.institucioneducativa, IDENTITY(iec.cicloTipo) as cicloId, e.fechaNacimiento as fechaNacimiento')
+                ->leftjoin('SieAppWebBundle:EstudianteInscripcion', 'ei', 'WITH', 'e.id = ei.estudiante')
+                ->leftjoin('SieAppWebBundle:InstitucioneducativaCurso', 'iec', 'WITH', 'ei.institucioneducativaCurso = iec.id')
+                ->leftjoin('SieAppWebBundle:Institucioneducativa', 'i', 'WITH', 'iec.institucioneducativa = i.id')
+                ->leftjoin('SieAppWebBundle:InstitucioneducativaTipo', 'it', 'WITH', 'i.institucioneducativaTipo = it.id')
+                ->leftjoin('SieAppWebBundle:NivelTipo', 'n', 'WITH', 'iec.nivelTipo = n.id')
+                ->leftjoin('SieAppWebBundle:GradoTipo', 'g', 'WITH', 'iec.gradoTipo = g.id')
+                ->leftjoin('SieAppWebBundle:ParaleloTipo', 'p', 'WITH', 'iec.paraleloTipo = p.id')
+                ->leftjoin('SieAppWebBundle:TurnoTipo', 't', 'WITH', 'iec.turnoTipo = t.id')
+                ->leftJoin('SieAppWebBundle:EstadoMatriculaTipo', 'em', 'WITH', 'ei.estadomatriculaTipo = em.id')
+                ->where('e.codigoRude = :id')
+                ->andWhere('it = :idTipo')
+                ->setParameter('id', $id)
+                ->setParameter('idTipo',1)
+                ->orderBy('iec.gestionTipo', 'DESC')
+                ->addorderBy('ei.fechaInscripcion', 'DESC')
+                ->getQuery();
+        try {
+            return $query->getResult();
+        } catch (Exception $ex) {
+            return $ex;
+        }
     }
 
 }
