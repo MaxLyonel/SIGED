0693afbacb75b76848767d52a20c3784f41b66c9
krlos pacha <pckrlos@gmail.com>
2018-01-31 10:24:35 -0400

    added EducacionDiversaTipo table - alternativa

---
+++ src/Sie/AppWebBundle/Entity/EducacionDiversaTipo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EducacionDiversaTipo
+ */
+class EducacionDiversaTipo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $educacionDiversa;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set educacionDiversa
+     *
+     * @param string $educacionDiversa
+     * @return EducacionDiversaTipo
+     */
+    public function setEducacionDiversa($educacionDiversa)
+    {
+        $this->educacionDiversa = $educacionDiversa;
+    
+        return $this;
+    }
+
+    /**
+     * Get educacionDiversa
+     *
+     * @return string 
+     */
+    public function getEducacionDiversa()
+    {
+        return $this->educacionDiversa;
+    }
+}
