b456c61b2e5a4b17f2ae8cb5cc6fc507e184b240
Jhonny <jhonnyq17@gmail.com>
2018-02-08 17:17:17 -0400

    Ajuste reinicia secuencia en estudiante asignatura - notas service

--- src/Sie/AppWebBundle/Services/Notas.php
+++ src/Sie/AppWebBundle/Services/Notas.php
@@ -239,10 +239,12 @@ class Notas{
                     $arrayAsignaturasEstudiante[] = $a['asignaturaId'];
                 }
 
-                $query = $this->em->getConnection()->prepare("select * from sp_reinicia_secuencia('estudiante_asignatura');")->execute();
+                
                 $nuevaArea = false;
                 foreach ($cursoOferta as $co) {
                     if(!in_array($co->getAsignaturaTipo()->getId(), $arrayAsignaturasEstudiante)){
+
+                        $query = $this->em->getConnection()->prepare("select * from sp_reinicia_secuencia('estudiante_asignatura');")->execute();
                         // Si no existe la asignatura, registramos la asignatura para el maestro
                         $newEstAsig = new EstudianteAsignatura();
                         $newEstAsig->setGestionTipo($this->em->getRepository('SieAppWebBundle:GestionTipo')->find($gestion));
