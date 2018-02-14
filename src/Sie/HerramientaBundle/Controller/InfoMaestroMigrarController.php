a4ddc0cd45e70bede8e0a34b942905a6eff1f733
Alejandro Fiengo <jose.fiengo.vega@gmail.com>
2018-02-14 18:37:20 -0400

    Actualización módulos: maestro/administrativo -academico

--- src/Sie/HerramientaBundle/Controller/InfoMaestroMigrarController.php
+++ src/Sie/HerramientaBundle/Controller/InfoMaestroMigrarController.php
@@ -82,7 +82,12 @@ class InfoMaestroMigrarController extends Controller {
         $consol_gest_pasada = $em->getRepository('SieAppWebBundle:RegistroConsolidacion')->findOneBy(array('gestion' => $request->getSession()->get('currentyear') - 1 , 'unidadEducativa' => $institucion, 'bim4' => '1'));
         $consol_gest_pasada2 = $em->getRepository('SieAppWebBundle:RegistroConsolidacion')->findOneBy(array('gestion' => $request->getSession()->get('currentyear') - 1 , 'unidadEducativa' => $institucion, 'bim4' => '2'));
         
-        if(!($consol_gest_pasada or $consol_gest_pasada2)){
+        $registro_gest_pasada = $em->getRepository('SieAppWebBundle:RegistroConsolidacion')->findOneBy(array('gestion' => $request->getSession()->get('currentyear') - 1 , 'unidadEducativa' => $institucion));
+        
+        if(!$registro_gest_pasada){
+            $gestion = $request->getSession()->get('currentyear');
+        }
+        else if(!($consol_gest_pasada or $consol_gest_pasada2)){
             $gestion = $request->getSession()->get('currentyear') - 1;
             $request->getSession()->set('idGestion', $gestion);
             $activar_acciones = true;
