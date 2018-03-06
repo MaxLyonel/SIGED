c21a7a2a010a0481ba9c49271047e9f1daf92707
krlos pacha <pckrlos@gmail.com>
2018-03-06 15:04:37 -0400

    generated the olimpiadas bundle - olimpiadas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -45,6 +45,7 @@ class AppKernel extends Kernel {
             new Nelmio\SecurityBundle\NelmioSecurityBundle(),
             new Sie\GisBundle\SieGisBundle(),
             new Sie\InfraBundle\SieInfraBundle(),
+            new Sie\OlimpiadasBundle\SieOlimpiadasBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
