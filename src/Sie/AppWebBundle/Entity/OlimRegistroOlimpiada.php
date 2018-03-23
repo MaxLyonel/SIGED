dbe673620ff2e7ad99c7e25595f0f190871207a1
krlos pacha <pckrlos@gmail.com>
2018-03-23 10:30:14 -0400

    mapped olimpiadas tables - olimpiadas

---
+++ src/Sie/AppWebBundle/Entity/OlimRegistroOlimpiada.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OlimRegistroOlimpiada
+ */
+class OlimRegistroOlimpiada
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombreOlimpiada;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaInicio;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaFin;
+
+    /**
+     * @var integer
+     */
+    private $gestionTipoId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombreOlimpiada
+     *
+     * @param string $nombreOlimpiada
+     * @return OlimRegistroOlimpiada
+     */
+    public function setNombreOlimpiada($nombreOlimpiada)
+    {
+        $this->nombreOlimpiada = $nombreOlimpiada;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombreOlimpiada
+     *
+     * @return string 
+     */
+    public function getNombreOlimpiada()
+    {
+        return $this->nombreOlimpiada;
+    }
+
+    /**
+     * Set fechaInicio
+     *
+     * @param \DateTime $fechaInicio
+     * @return OlimRegistroOlimpiada
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fechaInicio = $fechaInicio;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaInicio
+     *
+     * @return \DateTime 
+     */
+    public function getFechaInicio()
+    {
+        return $this->fechaInicio;
+    }
+
+    /**
+     * Set fechaFin
+     *
+     * @param \DateTime $fechaFin
+     * @return OlimRegistroOlimpiada
+     */
+    public function setFechaFin($fechaFin)
+    {
+        $this->fechaFin = $fechaFin;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaFin
+     *
+     * @return \DateTime 
+     */
+    public function getFechaFin()
+    {
+        return $this->fechaFin;
+    }
+
+    /**
+     * Set gestionTipoId
+     *
+     * @param integer $gestionTipoId
+     * @return OlimRegistroOlimpiada
+     */
+    public function setGestionTipoId($gestionTipoId)
+    {
+        $this->gestionTipoId = $gestionTipoId;
+    
+        return $this;
+    }
+
+    /**
+     * Get gestionTipoId
+     *
+     * @return integer 
+     */
+    public function getGestionTipoId()
+    {
+        return $this->gestionTipoId;
+    }
+}
