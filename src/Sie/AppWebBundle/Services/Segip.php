6e0602ec33bc5eb30bc4fe1073560c3f7e888906
José Alejandro Fiengo Vega <jose.fiengo.vega@gmail.com>
2018-07-05 09:42:08 -0400

    Cambio a entorno de prueba servicio segip

--- src/Sie/AppWebBundle/Services/Segip.php
+++ src/Sie/AppWebBundle/Services/Segip.php
@@ -20,7 +20,7 @@ class Segip {
         ]);
 
         /*TOKEN DE CONEXIÓN*/
-        $this->token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJXREdHc3VvQ1dVS1VFdDgxWFdnRk9NSnlBdzVRZDBxQyIsInVzZXIiOiJzaWVhY2FkZW1pY28iLCJleHAiOjE2MjUyMzg0MjB9.mZAIX3k76FkMxLKH8BlJ5CiGPlKEyKAFrsLTYG21Bqs';//'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJXREdHc3VvQ1dVS1VFdDgxWFdnRk9NSnlBdzVRZDBxQyIsInVzZXIiOiJzaWVhY2FkZW1pY28iLCJleHAiOjE2MjUyMzg0MjB9.mZAIX3k76FkMxLKH8BlJ5CiGPlKEyKAFrsLTYG21Bqs';//'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjODY5N25tY1M3TzViTmoxMFBEelI4OFY0QmtDQmlrVyIsInVzZXIiOiJhZmllbmdvIiwiZXhwIjoxNjE5OTgyMzAwfQ.sq_5MqNRGWl8x4DypeHJo-9_trLLFrYvF933B_8Niis';
+        $this->token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0M2t2ZVlDUmRxWGpFMW5QRHZYVWs1MW05OWEyOTJvQSIsInVzZXIiOiJzaXN0ZW1hc19kZXYiLCJleHAiOjE2MjUzNTAwMjB9.GmV5nakrvPSdrQq3TUAVwATtr4icHGE2Y2bj7w4qwSc';
         /*TOKEN DE CONEXIÓN*/
     }
     
@@ -28,7 +28,7 @@ class Segip {
 
         $response = $this->client->request(
             'GET', 
-            'segip/v2/status', 
+            'desarrollo/segip/v2/status', 
             ['headers' => ['Accept' => 'application/json', 'Authorization' => $this->token], 
             ['debug' => true]])->getBody()->getContents();
 
@@ -47,7 +47,7 @@ class Segip {
 
         $fechaNac = date('d/m/Y', strtotime($fechaNac));
 
-        $url = 'segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac;
+        $url = 'desarrollo/segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac;
 
         if($complemento != ''){
             $url = 'segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac.'&complemento='.$complemento;
@@ -68,7 +68,7 @@ class Segip {
 
         $fechaNac = date('d/m/Y', strtotime($fechaNac));
 
-        $url = 'segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac.'&primer_apellido='.$paterno.'&segundo_apellido='.$materno.'&nombre='.$nombre;
+        $url = 'desarrollo/segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac.'&primer_apellido='.$paterno.'&segundo_apellido='.$materno.'&nombre='.$nombre;
 
         if($complemento != ''){
             $url = 'segip/v2/personas/'.$carnet.'?fecha_nacimiento='.$fechaNac.'&primer_apellido='.$paterno.'&segundo_apellido='.$materno.'&nombre='.$nombre.'&complemento='.$complemento;
