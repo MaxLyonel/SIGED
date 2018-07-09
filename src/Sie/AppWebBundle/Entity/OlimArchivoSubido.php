d1dcd1a5ebbeb5053187a8033594ebbe27ef348f
krlos pacha <pckrlos@gmail.com>
2018-07-09 17:47:58 -0400

    mapped olim tables to the dev the second phase - olimpiada

---
+++ src/Sie/AppWebBundle/Entity/OlimArchivoSubido.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OlimArchivoSubido
+ */
+class OlimArchivoSubido
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $pathArchivo;
+
+    /**
+     * @var string
+     */
+    private $nombreArchivo;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaSubida;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\OlimEtapaTipo
+     */
+    private $olimEtapaTipo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set pathArchivo
+     *
+     * @param string $pathArchivo
+     * @return OlimArchivoSubido
+     */
+    public function setPathArchivo($pathArchivo)
+    {
+        $this->pathArchivo = $pathArchivo;
+    
+        return $this;
+    }
+
+    /**
+     * Get pathArchivo
+     *
+     * @return string 
+     */
+    public function getPathArchivo()
+    {
+        return $this->pathArchivo;
+    }
+
+    /**
+     * Set nombreArchivo
+     *
+     * @param string $nombreArchivo
+     * @return OlimArchivoSubido
+     */
+    public function setNombreArchivo($nombreArchivo)
+    {
+        $this->nombreArchivo = $nombreArchivo;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombreArchivo
+     *
+     * @return string 
+     */
+    public function getNombreArchivo()
+    {
+        return $this->nombreArchivo;
+    }
+
+    /**
+     * Set fechaSubida
+     *
+     * @param \DateTime $fechaSubida
+     * @return OlimArchivoSubido
+     */
+    public function setFechaSubida($fechaSubida)
+    {
+        $this->fechaSubida = $fechaSubida;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaSubida
+     *
+     * @return \DateTime 
+     */
+    public function getFechaSubida()
+    {
+        return $this->fechaSubida;
+    }
+
+    /**
+     * Set olimEtapaTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\OlimEtapaTipo $olimEtapaTipo
+     * @return OlimArchivoSubido
+     */
+    public function setOlimEtapaTipo(\Sie\AppWebBundle\Entity\OlimEtapaTipo $olimEtapaTipo = null)
+    {
+        $this->olimEtapaTipo = $olimEtapaTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get olimEtapaTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\OlimEtapaTipo 
+     */
+    public function getOlimEtapaTipo()
+    {
+        return $this->olimEtapaTipo;
+    }
+}
---
+++ src/Sie/AppWebBundle/Entity/OlimEtapaPeriodo.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OlimEtapaPeriodo
+ */
+class OlimEtapaPeriodo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaInicio;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaFin;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\OlimEtapaTipo
+     */
+    private $olimEtapaTipo;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\OlimRegistroOlimpiada
+     */
+    private $olimRegistroOlimpiada;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fechaInicio
+     *
+     * @param \DateTime $fechaInicio
+     * @return OlimEtapaPeriodo
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fechaInicio = $fechaInicio;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaInicio
+     *
+     * @return \DateTime 
+     */
+    public function getFechaInicio()
+    {
+        return $this->fechaInicio;
+    }
+
+    /**
+     * Set fechaFin
+     *
+     * @param \DateTime $fechaFin
+     * @return OlimEtapaPeriodo
+     */
+    public function setFechaFin($fechaFin)
+    {
+        $this->fechaFin = $fechaFin;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaFin
+     *
+     * @return \DateTime 
+     */
+    public function getFechaFin()
+    {
+        return $this->fechaFin;
+    }
+
+    /**
+     * Set olimEtapaTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\OlimEtapaTipo $olimEtapaTipo
+     * @return OlimEtapaPeriodo
+     */
+    public function setOlimEtapaTipo(\Sie\AppWebBundle\Entity\OlimEtapaTipo $olimEtapaTipo = null)
+    {
+        $this->olimEtapaTipo = $olimEtapaTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get olimEtapaTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\OlimEtapaTipo 
+     */
+    public function getOlimEtapaTipo()
+    {
+        return $this->olimEtapaTipo;
+    }
+
+    /**
+     * Set olimRegistroOlimpiada
+     *
+     * @param \Sie\AppWebBundle\Entity\OlimRegistroOlimpiada $olimRegistroOlimpiada
+     * @return OlimEtapaPeriodo
+     */
+    public function setOlimRegistroOlimpiada(\Sie\AppWebBundle\Entity\OlimRegistroOlimpiada $olimRegistroOlimpiada = null)
+    {
+        $this->olimRegistroOlimpiada = $olimRegistroOlimpiada;
+    
+        return $this;
+    }
+
+    /**
+     * Get olimRegistroOlimpiada
+     *
+     * @return \Sie\AppWebBundle\Entity\OlimRegistroOlimpiada 
+     */
+    public function getOlimRegistroOlimpiada()
+    {
+        return $this->olimRegistroOlimpiada;
+    }
+}
