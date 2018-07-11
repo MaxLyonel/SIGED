110efa5b701fbaece9ac526951528e0811fe830b
krlos pacha <pckrlos@gmail.com>
2018-07-11 09:07:56 -0400

    mapped olimmodalidadpruebatipo & olimmodalidadtipo tables - olimpiada

--- src/Sie/AppWebBundle/Entity/OlimModalidadPruebaTipo.php
+++ src/Sie/AppWebBundle/Entity/OlimModalidadPruebaTipo.php
@@ -80,4 +80,32 @@ class OlimModalidadPruebaTipo
     {
         return $this->olimRegistroOlimpiada;
     }
+    /**
+     * @var \Sie\AppWebBundle\Entity\OlimModalidadTipo
+     */
+    private $olimModalidadTipo;
+
+
+    /**
+     * Set olimModalidadTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\OlimModalidadTipo $olimModalidadTipo
+     * @return OlimModalidadPruebaTipo
+     */
+    public function setOlimModalidadTipo(\Sie\AppWebBundle\Entity\OlimModalidadTipo $olimModalidadTipo = null)
+    {
+        $this->olimModalidadTipo = $olimModalidadTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get olimModalidadTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\OlimModalidadTipo 
+     */
+    public function getOlimModalidadTipo()
+    {
+        return $this->olimModalidadTipo;
+    }
 }
