a3c1949384b8765f5b9c5d3105e6abc681921c6c
krlos pacha <pckrlos@gmail.com>
2018-07-31 16:49:14 -0400

    mapped wf tables to worflow options - procesos

--- src/Sie/AppWebBundle/Entity/FlujoProceso.php
+++ src/Sie/AppWebBundle/Entity/FlujoProceso.php
@@ -164,4 +164,200 @@ class FlujoProceso
     {
         return $this->rolTipo;
     }
+    /**
+     * @var integer
+     */
+    private $procesoId;
+
+    /**
+     * @var boolean
+     */
+    private $esEvaluacion;
+
+    /**
+     * @var integer
+     */
+    private $plazo;
+
+    /**
+     * @var integer
+     */
+    private $tareaAntId;
+
+    /**
+     * @var integer
+     */
+    private $tareaSigId;
+
+    /**
+     * @var string
+     */
+    private $variableEvaluacion;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\WfAsignacionTareaTipo
+     */
+    private $wfAsignacionTareaTipo;
+
+
+    /**
+     * Set procesoId
+     *
+     * @param integer $procesoId
+     * @return FlujoProceso
+     */
+    public function setProcesoId($procesoId)
+    {
+        $this->procesoId = $procesoId;
+    
+        return $this;
+    }
+
+    /**
+     * Get procesoId
+     *
+     * @return integer 
+     */
+    public function getProcesoId()
+    {
+        return $this->procesoId;
+    }
+
+    /**
+     * Set esEvaluacion
+     *
+     * @param boolean $esEvaluacion
+     * @return FlujoProceso
+     */
+    public function setEsEvaluacion($esEvaluacion)
+    {
+        $this->esEvaluacion = $esEvaluacion;
+    
+        return $this;
+    }
+
+    /**
+     * Get esEvaluacion
+     *
+     * @return boolean 
+     */
+    public function getEsEvaluacion()
+    {
+        return $this->esEvaluacion;
+    }
+
+    /**
+     * Set plazo
+     *
+     * @param integer $plazo
+     * @return FlujoProceso
+     */
+    public function setPlazo($plazo)
+    {
+        $this->plazo = $plazo;
+    
+        return $this;
+    }
+
+    /**
+     * Get plazo
+     *
+     * @return integer 
+     */
+    public function getPlazo()
+    {
+        return $this->plazo;
+    }
+
+    /**
+     * Set tareaAntId
+     *
+     * @param integer $tareaAntId
+     * @return FlujoProceso
+     */
+    public function setTareaAntId($tareaAntId)
+    {
+        $this->tareaAntId = $tareaAntId;
+    
+        return $this;
+    }
+
+    /**
+     * Get tareaAntId
+     *
+     * @return integer 
+     */
+    public function getTareaAntId()
+    {
+        return $this->tareaAntId;
+    }
+
+    /**
+     * Set tareaSigId
+     *
+     * @param integer $tareaSigId
+     * @return FlujoProceso
+     */
+    public function setTareaSigId($tareaSigId)
+    {
+        $this->tareaSigId = $tareaSigId;
+    
+        return $this;
+    }
+
+    /**
+     * Get tareaSigId
+     *
+     * @return integer 
+     */
+    public function getTareaSigId()
+    {
+        return $this->tareaSigId;
+    }
+
+    /**
+     * Set variableEvaluacion
+     *
+     * @param string $variableEvaluacion
+     * @return FlujoProceso
+     */
+    public function setVariableEvaluacion($variableEvaluacion)
+    {
+        $this->variableEvaluacion = $variableEvaluacion;
+    
+        return $this;
+    }
+
+    /**
+     * Get variableEvaluacion
+     *
+     * @return string 
+     */
+    public function getVariableEvaluacion()
+    {
+        return $this->variableEvaluacion;
+    }
+
+    /**
+     * Set wfAsignacionTareaTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\WfAsignacionTareaTipo $wfAsignacionTareaTipo
+     * @return FlujoProceso
+     */
+    public function setWfAsignacionTareaTipo(\Sie\AppWebBundle\Entity\WfAsignacionTareaTipo $wfAsignacionTareaTipo = null)
+    {
+        $this->wfAsignacionTareaTipo = $wfAsignacionTareaTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get wfAsignacionTareaTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\WfAsignacionTareaTipo 
+     */
+    public function getWfAsignacionTareaTipo()
+    {
+        return $this->wfAsignacionTareaTipo;
+    }
 }
---
+++ src/Sie/AppWebBundle/Entity/WfPasosTipo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * WfPasosTipo
+ */
+class WfPasosTipo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return WfPasosTipo
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
