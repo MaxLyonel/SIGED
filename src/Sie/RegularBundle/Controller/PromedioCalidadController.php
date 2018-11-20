7b9d035864df95d034f0e862437f6af38e5b9b5b
Jhonny <jhonnyq17@gmail.com>
2018-11-20 11:07:19 -0400

    Modulo de correccion de promedio fisica quimica - siged

---
+++ src/Sie/RegularBundle/Controller/PromedioCalidadController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Sie\RegularBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityRepository;
+use Sie\AppWebBundle\Entity\InstitucioneducativaCurso;
+use Sie\AppWebBundle\Entity\InstitucioneducativaCursoOferta;
+use Sie\AppWebBundle\Entity\InstitucioneducativaCursoOfertaMaestro;
+use Sie\AppWebBundle\Entity\EstudianteAsignatura;
+use Sie\AppWebBundle\Entity\EstudianteNota;
+
+/**
+ * EstudianteInscripcion controller.
+ *
+ */
+class PromedioCalidadController extends Controller {
+
+    public $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+
+    /**
+     * Datos del estudiante y de sus materias fisica quimica
+     */
+    public function indexAction(Request $request) {
+        $form = $request->get('form');
+        $idObservacion = $form['idDetalle'];
+        $sie = $form['institucioneducativa'];
+        $gestion = $form['gestion'];
+        $llave = $form['llave'];
+        $parametros = explode('|', $llave);
+
+        $idInscripcion = $parametros[0];//$request->get('idInscripcion');
+        $idNota = $parametros[1];//$request->get('idNota');
+        $idNotaTipo = $parametros[2];//$request->get('idNotaTipo');
+        $promedio = $parametros[3];
+
+        $em = $this->getDoctrine()->getManager();
+        $inscripcion = $em->getRepository('SieAppWebBundle:EstudianteInscripcion')->find($idInscripcion);
+        $nota = $em->getRepository('SieAppWebBundle:EstudianteNota')->find($idNota);
+
+        $notas = $em->createQueryBuilder()
+                    ->select('at.id as codigo, at.asignatura, en.id, en.notaCuantitativa')
+                    ->from('SieAppWebBundle:EstudianteAsignatura','ea')
+                    ->innerJoin('SieAppWebBundle:EstudianteNota','en','with','en.estudianteAsignatura = ea.id')
+                    ->innerJoin('SieAppWebBundle:NotaTipo','nt','with','en.notaTipo = nt.id')
+                    ->innerJoin('SieAppWebBundle:InstitucioneducativaCursoOferta','ieco','with','ea.institucioneducativaCursoOferta = ieco.id')
+                    ->innerJoin('SieAppWebBundle:AsignaturaTipo','at','with','ieco.asignaturaTipo = at.id')
+                    ->where('ea.estudianteInscripcion = :idInscripcion')
+                    ->andWhere('at.id IN (:ids)')
+                    ->andWhere('nt.id = :idNotaTipo')
+                    ->setParameter('idInscripcion', $idInscripcion)
+                    ->setParameter('ids', array(1051, 1052, 1053))
+                    ->setParameter('idNotaTipo', $idNotaTipo)
+                    ->orderBy('at.id','DESC')
+                    ->getQuery()
+                    ->getResult();
+
+        $promedioCalculado = 0;
+        $suma = 0;
+        foreach ($notas as $n) {
+            if($n['codigo'] != 1051){
+                $suma = $suma + $n['notaCuantitativa'];
+            }
+        }
+
+        $observacion = $em->getRepository('SieAppWebBundle:ValidacionProceso')->find($idObservacion);
+
+
+        $promedioCalculado = round($suma/2);
+
+        return $this->render('SieRegularBundle:PromedioCalidad:index.html.twig', [
+            'inscripcion'=>$inscripcion,
+            'nota'=>$nota,
+            'notas'=>$notas,
+            'promedio'=>$promedioCalculado,
+            'idObservacion'=>$idObservacion,
+            'observacion'=>$observacion,
+            'bimestre'=>$idNotaTipo,
+            'sie'=>$sie,
+            'gestion'=>$gestion
+        ]);
+
+    }
+
+    /*
+     * Save cambios 
+     */
+
+    public function saveAction(Request $request) {
+
+        // dump($request);die;
+        $idNota = $request->get('idNota');
+        $promedio = $request->get('promedio');
+        $acuerdo = $request->get('acuerdo');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $idObservacion = $request->get('idObservacion');
+        $observacion = $em->getRepository('SieAppWebBundle:ValidacionProceso')->find($idObservacion);
+
+        if($acuerdo == "1"){
+            $nota = $em->getRepository('SieAppWebBundle:EstudianteNota')->find(intval($idNota));
+            $nota->setNotaCuantitativa($promedio);
+            $em->flush();
+
+            $observacion->setEsActivo(true);
+            $em->flush();
+
+        }else{
+            $observacion->setSolucionTipoId(1);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('ccalidad_index');
+    }
+}
