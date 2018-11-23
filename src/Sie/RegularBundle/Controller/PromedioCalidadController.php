51a496a9153ca76c0efd53f4f996c58d1a99abb8
Jhonny <jhonnyq17@gmail.com>
2018-11-23 17:03:11 -0400

    Ajuste promedio calidad fisica quimica fecha corte -siged

--- src/Sie/RegularBundle/Controller/PromedioCalidadController.php
+++ src/Sie/RegularBundle/Controller/PromedioCalidadController.php
@@ -151,6 +151,16 @@ class PromedioCalidadController extends Controller {
             return $this->redirect($this->generateUrl('login'));
         }
 
+        // redireccionamos si se excedio la fecha de corte del modulo
+        $fechaActual = new \DateTime('now');
+        $fechaCorte = new \DateTime('2018-11-24');
+
+        if($fechaActual > $fechaCorte) {
+            return $this->redirect($this->generateUrl('principal_web'));
+        }
+
+
+
         $rol_usuario = $this->session->get('roluser');
 
         if ($rol_usuario != '9') {
