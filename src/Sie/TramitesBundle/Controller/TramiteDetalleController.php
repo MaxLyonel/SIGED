1641f9c721c0a3a67f0bbc9854bd50124bf82a7f
RolyCanaviri <jurlan.rol@gmail.com>
2018-11-23 17:08:22 -0400

    habilitacion de modificacion de fecha de emision para certificaciones 2 - tramite

--- src/Sie/TramitesBundle/Controller/TramiteDetalleController.php
+++ src/Sie/TramitesBundle/Controller/TramiteDetalleController.php
@@ -1632,7 +1632,7 @@ class TramiteDetalleController extends Controller {
                 $numeroCarton = $request->get('numeroSerie');
                 $serieCarton = $request->get('serie');
                 //$gestionCarton = $request->get('gestion');
-                $fechaCarton = $request->get('fecha');
+                $fechaCarton = new \DateTime($request->get('fecha'));
                 //$fechaCarton = $fechaActual;
 
                 $token = $request->get('_token');
