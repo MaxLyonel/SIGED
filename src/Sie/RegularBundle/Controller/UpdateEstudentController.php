f984b69455bf6fd58eb8c1923e58381ec6a7e224
krlos <pckrlos@gmail.com>
2018-11-28 15:36:49 -0400

    change segipId value from 20 to 1 in update data student modules - siged

--- src/Sie/RegularBundle/Controller/UpdateEstudentController.php
+++ src/Sie/RegularBundle/Controller/UpdateEstudentController.php
@@ -142,7 +142,7 @@ class UpdateEstudentController  extends Controller{
     
         $student = $em->getRepository('SieAppWebBundle:Estudiante')->find($form['id']);
         if($answerSegip===true){
-            $student->setSegipId(20);       
+            $student->setSegipId(1);       
         }else{
             $student->setSegipId(0);       
         }
