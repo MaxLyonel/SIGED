0f0a069cb9cef6b904ea341364e039b30e63ace0
krlos <pckrlos@gmail.com>
2019-03-01 11:36:52 -0400

    mapped jdp tables - juegos

---
+++ src/Sie/AppWebBundle/Entity/JdpModalidadTipo.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * JdpModalidadTipo
+ */
+class JdpModalidadTipo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $modalidad;
+
+    /**
+     * @var string
+     */
+    private $obs;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set modalidad
+     *
+     * @param string $modalidad
+     * @return JdpModalidadTipo
+     */
+    public function setModalidad($modalidad)
+    {
+        $this->modalidad = $modalidad;
+    
+        return $this;
+    }
+
+    /**
+     * Get modalidad
+     *
+     * @return string 
+     */
+    public function getModalidad()
+    {
+        return $this->modalidad;
+    }
+
+    /**
+     * Set obs
+     *
+     * @param string $obs
+     * @return JdpModalidadTipo
+     */
+    public function setObs($obs)
+    {
+        $this->obs = $obs;
+    
+        return $this;
+    }
+
+    /**
+     * Get obs
+     *
+     * @return string 
+     */
+    public function getObs()
+    {
+        return $this->obs;
+    }
+}
---
+++ src/Sie/AppWebBundle/Entity/JdpPruebaTipo.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * JdpPruebaTipo
+ */
+class JdpPruebaTipo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $prueba;
+
+    /**
+     * @var string
+     */
+    private $clasificadorPuntuacion;
+
+    /**
+     * @var string
+     */
+    private $clasificadorTipo;
+
+    /**
+     * @var boolean
+     */
+    private $esactivo;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\DisciplinaTipo
+     */
+    private $disciplinaTipo;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\GeneroTipo
+     */
+    private $generoTipo;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\JdpPruebaParticipacionTipo
+     */
+    private $pruebaParticipacionTipo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set prueba
+     *
+     * @param string $prueba
+     * @return JdpPruebaTipo
+     */
+    public function setPrueba($prueba)
+    {
+        $this->prueba = $prueba;
+    
+        return $this;
+    }
+
+    /**
+     * Get prueba
+     *
+     * @return string 
+     */
+    public function getPrueba()
+    {
+        return $this->prueba;
+    }
+
+    /**
+     * Set clasificadorPuntuacion
+     *
+     * @param string $clasificadorPuntuacion
+     * @return JdpPruebaTipo
+     */
+    public function setClasificadorPuntuacion($clasificadorPuntuacion)
+    {
+        $this->clasificadorPuntuacion = $clasificadorPuntuacion;
+    
+        return $this;
+    }
+
+    /**
+     * Get clasificadorPuntuacion
+     *
+     * @return string 
+     */
+    public function getClasificadorPuntuacion()
+    {
+        return $this->clasificadorPuntuacion;
+    }
+
+    /**
+     * Set clasificadorTipo
+     *
+     * @param string $clasificadorTipo
+     * @return JdpPruebaTipo
+     */
+    public function setClasificadorTipo($clasificadorTipo)
+    {
+        $this->clasificadorTipo = $clasificadorTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get clasificadorTipo
+     *
+     * @return string 
+     */
+    public function getClasificadorTipo()
+    {
+        return $this->clasificadorTipo;
+    }
+
+    /**
+     * Set esactivo
+     *
+     * @param boolean $esactivo
+     * @return JdpPruebaTipo
+     */
+    public function setEsactivo($esactivo)
+    {
+        $this->esactivo = $esactivo;
+    
+        return $this;
+    }
+
+    /**
+     * Get esactivo
+     *
+     * @return boolean 
+     */
+    public function getEsactivo()
+    {
+        return $this->esactivo;
+    }
+
+    /**
+     * Set disciplinaTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\DisciplinaTipo $disciplinaTipo
+     * @return JdpPruebaTipo
+     */
+    public function setDisciplinaTipo(\Sie\AppWebBundle\Entity\DisciplinaTipo $disciplinaTipo = null)
+    {
+        $this->disciplinaTipo = $disciplinaTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get disciplinaTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\DisciplinaTipo 
+     */
+    public function getDisciplinaTipo()
+    {
+        return $this->disciplinaTipo;
+    }
+
+    /**
+     * Set generoTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\GeneroTipo $generoTipo
+     * @return JdpPruebaTipo
+     */
+    public function setGeneroTipo(\Sie\AppWebBundle\Entity\GeneroTipo $generoTipo = null)
+    {
+        $this->generoTipo = $generoTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get generoTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\GeneroTipo 
+     */
+    public function getGeneroTipo()
+    {
+        return $this->generoTipo;
+    }
+
+    /**
+     * Set pruebaParticipacionTipo
+     *
+     * @param \Sie\AppWebBundle\Entity\JdpPruebaParticipacionTipo $pruebaParticipacionTipo
+     * @return JdpPruebaTipo
+     */
+    public function setPruebaParticipacionTipo(\Sie\AppWebBundle\Entity\JdpPruebaParticipacionTipo $pruebaParticipacionTipo = null)
+    {
+        $this->pruebaParticipacionTipo = $pruebaParticipacionTipo;
+    
+        return $this;
+    }
+
+    /**
+     * Get pruebaParticipacionTipo
+     *
+     * @return \Sie\AppWebBundle\Entity\JdpPruebaParticipacionTipo 
+     */
+    public function getPruebaParticipacionTipo()
+    {
+        return $this->pruebaParticipacionTipo;
+    }
+}
