0c29be88f21e6ec20154eccf8c6f21b8e5bbf55b
patricia <patriciavargash1@gmail.com>
2019-03-22 12:56:14 -0400

    validacion idpersona-DEFAULT

--- src/Sie/AppWebBundle/Controller/DefaultController.php
+++ src/Sie/AppWebBundle/Controller/DefaultController.php
@@ -471,6 +471,7 @@ class DefaultController extends Controller {
             if ( $user and is_object($user) ) {//USUARIO Y CONTRASEÑA CORRECTAS
                 //*******SE VERIFICA SI SE TRATA DE RESETEO DE CONTRASEÑA
                 $this->session->set('userId', $user->getId());
+                
                 if (md5($user->getUsername()) == $user->getPassword()) {
                     return $this->redirect($this->generateUrl('sie_usuarios_reset_login', array('usuarioid' => $user->getId())));
                 }
@@ -715,6 +716,7 @@ class DefaultController extends Controller {
                     }
 
                     if (($exp == 'true') || ($carnetban == 'true') || ($mendir == 'true')){
+                        //dump($rolselected);die;
                         return $this->render('SieAppWebBundle:Login:rolesunidades.html.twig',
                         array(
                             'titulosubsistema' => $this->session->get('sysname'),
@@ -730,6 +732,7 @@ class DefaultController extends Controller {
                     //CUANDO EL USUARIO SOLO TIENE UN ROL ACTIVO
                     //SE ENVIA AL CONTROLADOR LOGIN PARA ULTIMAS VERIFICACIONES
                     $sesion->set('directorAlternativa', false);
+                    //dump($rolselected);die;
                     if (count($rolselected) == 1) {
                         if ( ($rolselected[0]['id'] == 2) || ($rolselected[0]['id'] == 9) ){
                             $this->session->set('roluser', $rolselected[0]['id']);
@@ -759,6 +762,7 @@ class DefaultController extends Controller {
                             $this->session->set('cuentauser', $rolselected[0]['rol']);
                             $this->session->set('tiposubsistema', $rolselected[0]['idietipo']);
                         }
+                        
                         return $this->redirect($this->generateUrl('sie_login_homepage'));
                     }
                     //FIN DE CUANDO EL USUARIO SOLO TIENE UN ROL ACTIVO
@@ -809,6 +813,11 @@ class DefaultController extends Controller {
     public function sigedrolselectAction(Request $request, $key) {
         $em =  $this->getDoctrine()->getManager();
         $sesion = $request->getSession();
+        //$id_usuario = $this->sesion->get('userId');
+        //dump($sesion);die;
+        if (!$sesion->get('personaId')) {
+            return $this->redirect($this->generateUrl('login'));
+        }
         $rolselected = $this->get('login')->verificarRolesActivos($sesion->get('personaId'),$key);
         //dump($rolselected);
         //die;
