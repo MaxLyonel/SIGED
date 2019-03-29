e5ef2ae1d4105494d101700cc4f6adbc553707ac
Guadalupe <gquisbertfernandez@gmail.com>
2019-03-29 17:12:49 -0400

    se comento la funcion menu en login -menu

--- src/Sie/AppWebBundle/Controller/LoginController.php
+++ src/Sie/AppWebBundle/Controller/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller {
         //****FIN DE SE OBTIENE EL ID DEL ROL DEL USUARIO 
         //******************
                
-
+/*
         //****************
         //****SE GENERAN LOS MENUS PARA EL SIGEN EN BASE AL ID DEL ROL DEL USUARIO
         $query = $em->getConnection()->prepare('SELECT get_objeto_menu (:rol_id::INT)');
@@ -87,10 +87,10 @@ class LoginController extends Controller {
             //set some values fot the view template
             //$sesion->set('aMenuOption', $aBuildMenu);
             $sesion->set('aMenu', $optionMenu);
-        }
+        }*/
         //****FIN SE GENERAN LOS MENUS PARA EL SIGED EN BASE AL ID DEL ROL DEL USUARIO
         //****************
-        
+
         //****************
         //****REDIRECCIONA SEGUN EL TIPO DE SUBSISTEMA 
         /*switch ($sesion->get('tiposubsistema')) {
