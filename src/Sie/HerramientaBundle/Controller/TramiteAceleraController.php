887959136ae0b1d339594bbe99c025dd9b605f64
Adalid <ren.webmobile@gmail.com>
2019-06-24 11:59:48 -0400

    Reportes de comprobantes integrado - Siged

--- src/Sie/HerramientaBundle/Controller/TramiteAceleraController.php
+++ src/Sie/HerramientaBundle/Controller/TramiteAceleraController.php
@@ -878,4 +878,65 @@ class TramiteAceleraController extends Controller
             return $ex;
         }
     }
+
+    public function rptSolicitudAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $tramite_id = $request->get('idtramite');
+        $tareadetalle_id = $request->get('id_td');
+
+        $tramite = $em->getRepository('SieAppWebBundle:Tramite')->findOneById($tramite_id);
+        $institucioneducativa_id = $tramite->getInstitucioneducativa()->getId();
+        $gestion_id = $tramite->getGestionId();
+
+        $resultDatos = $em->getRepository('SieAppWebBundle:WfSolicitudTramite')->createQueryBuilder('wfd')
+            ->select('wfd')
+            ->innerJoin('SieAppWebBundle:TramiteDetalle', 'td', 'with', 'td.id = wfd.tramiteDetalle')
+            ->where('td.id='.$tareadetalle_id)
+            ->getQuery()
+            ->getSingleResult();
+        $estudiante_id = json_decode($resultDatos->getdatos())->estudiante_id;
+        $filename = $institucioneducativa_id.'TA_Solicitud_'.$gestion_id. '_'.date('YmdHis').'.pdf';
+
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/pdf');
+        $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $filename));
+        $response->setContent(file_get_contents($this->container->getParameter('urlreportweb') . 'aceleracion_educativa_solicitud_v1_amg.rptdesign&&institucioneducativa_id='.$institucioneducativa_id.'&&gestion_id='.$gestion_id.'&&estudiante_id='.$estudiante_id.'&&tramite_id='.$tramite_id.'&&__format=pdf&'));
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        return $response;
+    }
+
+    public function rptSupletorioAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $tramite_id = $request->get('idtramite');
+        $tareadetalle_id = $request->get('id_td');
+
+        $tramite = $em->getRepository('SieAppWebBundle:Tramite')->findOneById($tramite_id);
+        $institucioneducativa_id = $tramite->getInstitucioneducativa()->getId();
+        $gestion_id = $tramite->getGestionId();
+
+        $resultDatos = $em->getRepository('SieAppWebBundle:WfSolicitudTramite')->createQueryBuilder('wfd')
+            ->select('wfd')
+            ->innerJoin('SieAppWebBundle:TramiteDetalle', 'td', 'with', 'td.id = wfd.tramiteDetalle')
+            ->innerJoin('SieAppWebBundle:FlujoProceso', 'fp', 'with', 'td.flujoProceso = fp.id')
+            ->where('td.tramite='.$tramite_id)
+            ->andWhere('fp.orden=1')
+            ->andWhere("wfd.esValido=true")
+            ->orderBy("td.flujoProceso")
+            ->getQuery()
+            ->getSingleResult();
+        $estudiante_id = json_decode($resultDatos->getdatos())->estudiante_id;
+        $filename = $institucioneducativa_id.'TA_Supletorio_'.$gestion_id. '_'.date('YmdHis').'.pdf';
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/pdf');
+        $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $filename));
+        $response->setContent(file_get_contents($this->container->getParameter('urlreportweb') . 'aceleracion_educativa_supletorio_v1_amg.rptdesign&&institucioneducativa_id='.$institucioneducativa_id.'&&gestion_id='.$gestion_id.'&&estudiante_id='.$estudiante_id.'&&tramite_id='.$tramite_id.'&&__format=pdf&'));
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        return $response;
+    }
 }
