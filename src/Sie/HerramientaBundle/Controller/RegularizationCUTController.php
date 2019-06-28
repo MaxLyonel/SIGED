273bcc90bc881512bd4fc603829f373eac995f77
Jhonny <jhonnyq17@gmail.com>
2019-06-28 15:53:08 -0400

    Ajustes modulo nivelacion - academico

--- src/Sie/HerramientaBundle/Controller/RegularizationCUTController.php
+++ src/Sie/HerramientaBundle/Controller/RegularizationCUTController.php
@@ -111,7 +111,7 @@ class RegularizationCUTController extends Controller{
         $objStudent = $em->getRepository('SieAppWebBundle:Estudiante')->findOneBy(array('codigoRude'=>trim($form['codigoRude'])));        
         if($objStudent){
             // check if the student is in 6t
-            $dataStudentSixth      = $this->get('funciones')->getInscriptionBthByRude($objStudent->getCodigoRude());
+            // $dataStudentSixth      = $this->get('funciones')->getInscriptionBthByRude($objStudent->getCodigoRude());
             $objStudentInscription = $this->get('funciones')->getInscriptionBthByGestion($form);
             // dump($objStudentInscription);die;
 
@@ -121,46 +121,60 @@ class RegularizationCUTController extends Controller{
                 $swError = false;
             }else{
                 if($objStudentInscription){
-                        // check if the user has permissions
-                        $query = $em->getConnection()->prepare('SELECT get_ue_tuicion (:user_id::INT, :sie::INT, :rolId::INT)');
-                        $query->bindValue(':user_id', $this->session->get('userId'));
-                        $query->bindValue(':sie', $dataStudentSixth[0]['sie']);
-                        $query->bindValue(':rolId', $this->session->get('roluser'));
-                        $query->execute();
-                        $arrTuicion = $query->fetchAll();
-                        // check if the user has permissions on the student
-                        if ($arrTuicion[0]['get_ue_tuicion']) {
 
-                               if(sizeof($dataStudentSixth)>0){
-                                    // check if the student is not avalible to BTH
-                                    $objInscriptionTecnicoHumanistico = $em->getRepository('SieAppWebBundle:EstudianteInscripcionHumnisticoTecnico')->findOneBy(array(
-                                      'estudianteInscripcion' => $dataStudentSixth[0]['estInsId']
-                                  ));
-                                    // if($objInscriptionTecnicoHumanistico && $objInscriptionTecnicoHumanistico->getEsValido()==false){
-                                    if(true){
-                                        
+                        // VALIDAR SI LA INSCRIPCION CORRESPONDE A UN SIE AUTORIZADO
+                        $sie = $objStudentInscription[0]['sie'];
+                        
+                        $siesAutorizados = array(80730032);
+
+                        if(in_array($sie, $siesAutorizados)){
+
+                            // check if the user has permissions
+                            $query = $em->getConnection()->prepare('SELECT get_ue_tuicion (:user_id::INT, :sie::INT, :rolId::INT)');
+                            $query->bindValue(':user_id', $this->session->get('userId'));
+                            $query->bindValue(':sie', $objStudentInscription[0]['sie']);
+                            $query->bindValue(':rolId', $this->session->get('roluser'));
+                            $query->execute();
+                            $arrTuicion = $query->fetchAll();
+                            // check if the user has permissions on the student
+                            if ($arrTuicion[0]['get_ue_tuicion']) {
+
+                                   if(sizeof($objStudentInscription)>0){
+                                        // check if the student is not avalible to BTH
+                                        $objInscriptionTecnicoHumanistico = $em->getRepository('SieAppWebBundle:EstudianteInscripcionHumnisticoTecnico')->findOneBy(array(
+                                          'estudianteInscripcion' => $objStudentInscription[0]['estInsId']
+                                      ));
+                                        // if($objInscriptionTecnicoHumanistico && $objInscriptionTecnicoHumanistico->getEsValido()==false){
+                                        if(true){
+                                            
+                                        }else{
+                                             $message = 'Estudiante habilitado para la descarga del CUT';
+                                            $this->addFlash('warningReg', $message);
+                                            $swError = false;
+                                        }
+
                                     }else{
-                                         $message = 'Estudiante habilitado para la descarga del CUT';
+                                        $message = 'El Estudiante no esta en el nivel requerido';
                                         $this->addFlash('warningReg', $message);
                                         $swError = false;
+
                                     }
 
-                                }else{
-                                    $message = 'El Estudiante no esta en el nivel requerido';
-                                    $this->addFlash('warningReg', $message);
-                                    $swError = false;
+                            }else{
+                                 $message = 'No tiene tuición para continuar con el ragistro';
+                                $this->addFlash('warningReg', $message);
+                                $swError = false;
 
-                                }
-
+                            }
+                            
                         }else{
-                             $message = 'No tiene tuición para continuar con el ragistro';
+                            $message = 'El codigo SIE no esta autorizado';
                             $this->addFlash('warningReg', $message);
                             $swError = false;
-
                         }
 
                 }else{
-                    $message = 'Estudiante no se encuentra en 6to de Seccundaria';
+                    $message = 'Estudiante no tiene inscripcion';
                     $this->addFlash('warningReg', $message);
                     $swError = false;
                 }
