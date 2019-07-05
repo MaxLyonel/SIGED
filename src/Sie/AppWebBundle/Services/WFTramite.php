5def266cac7385c5695e2fbb4c647fd5c7bf6fe6
Patricia <patriciavargash1@gmail.com>
2019-07-05 09:18:27 -0400

    validacion usuario remitente-procesos

--- src/Sie/AppWebBundle/Services/WFTramite.php
+++ src/Sie/AppWebBundle/Services/WFTramite.php
@@ -154,6 +154,12 @@ class WFTramite {
         $tramite = $this->em->getRepository('SieAppWebBundle:Tramite')->find($idtramite);
         $tramiteDetalle = $this->em->getRepository('SieAppWebBundle:TramiteDetalle')->find((int)$tramite->getTramite());
         $this->em->getConnection()->beginTransaction();
+
+        if($tramiteDetalle->getUsuarioRemitente()->getId() == $usuario){
+            $mensaje['dato'] = false;
+            $mensaje['msg'] = '¡Error, tramite no enviado pues el usuario remitente no corresponde.!';
+            return $mensaje;
+        }
         try {
             /**
             * asigana usuario destinatario
