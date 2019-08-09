5ba39f9f08c884b895c078955efdff7d14ff10e6
krlos <pckrlos@gmail.com>
2019-08-09 12:08:18 -0400

    redirect to index to see the new postbachillerato option - siged

--- src/Sie/RegularBundle/Controller/PostBachilleratoController.php
+++ src/Sie/RegularBundle/Controller/PostBachilleratoController.php
@@ -36,7 +36,10 @@ class PostBachilleratoController extends Controller {
      *
      */
     public function indexAction() {
-        //die('krlos');
+        
+        $this->session->getFlashBag()->add('successpost', 'Use el modulo Extranjeros/Inicial/Post Bachillerato...');
+        return $this->redirect($this->generateUrl('post_bachillerato_index'));
+        die('krlos');
         $em = $this->getDoctrine()->getManager();
 
         $id_usuario = $this->session->get('userId');
