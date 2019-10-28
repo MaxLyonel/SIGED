041e5c25d378b66e5b4127e193dae4a85f448ade
José Alejandro Fiengo Vega <jose.fiengo.vega@gmail.com>
2019-10-28 10:33:18 -0400

    Actualización token api.sie.gob.bo

--- src/Sie/AppWebBundle/Services/Persona.php
+++ src/Sie/AppWebBundle/Services/Persona.php
@@ -18,10 +18,9 @@ class Persona {
 
     public function buscarPersona($carnet, $complemento, $extranjero) {
 
-        /***NUEVO TOKEN A LA FECHA 25/09/2017*/
-        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU0MDQ5MTY3OSwiZXhwIjoxNTcyMDI3Njc5fQ.lf233gXhht6PCcs29WmKN9a2Sy-sj_buKtJVsbM_CPc';
-
-        /***NUEVO TOKEN A LA FECHA 16/6/2017*/
+        /***NUEVO TOKEN A LA FECHA 28/10/2019*/
+        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU3MjI3MzAzMiwiZXhwIjoxNjAzODA5MDMyfQ.F4Vu2jxfQzuZDWWR5wD93aRNTKKTfMYTVE2m4Wu8GIQ';
+        /***NUEVO TOKEN A LA FECHA 28/10/2019*/
         
 
         $client = new Client([
@@ -41,8 +40,8 @@ class Persona {
 	}
 
     public function BuscarPersonaPorCarnetComplementoFechaNacimiento($carnet, $complemento, $fechaNacimiento) {
-        /***NUEVO TOKEN A LA FECHA 25/09/2017*/
-        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU0MDQ5MTY3OSwiZXhwIjoxNTcyMDI3Njc5fQ.lf233gXhht6PCcs29WmKN9a2Sy-sj_buKtJVsbM_CPc';
+        /***NUEVO TOKEN A LA FECHA 28/10/2019*/
+        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU3MjI3MzAzMiwiZXhwIjoxNjAzODA5MDMyfQ.F4Vu2jxfQzuZDWWR5wD93aRNTKKTfMYTVE2m4Wu8GIQ';
 
         $client = new Client([
         // Base URI is used with relative requests
@@ -61,8 +60,8 @@ class Persona {
 	}
 
     public function registrarPersona($carnet, $complemento, $fechanacimiento, $paterno, $materno, $nombre, $genero) {
-        /***NUEVO TOKEN A LA FECHA 25/09/2017*/
-        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU0MDQ5MTY3OSwiZXhwIjoxNTcyMDI3Njc5fQ.lf233gXhht6PCcs29WmKN9a2Sy-sj_buKtJVsbM_CPc';
+        /***NUEVO TOKEN A LA FECHA 28/10/2019*/
+        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU3MjI3MzAzMiwiZXhwIjoxNjAzODA5MDMyfQ.F4Vu2jxfQzuZDWWR5wD93aRNTKKTfMYTVE2m4Wu8GIQ';
 
         $client = new Client([
         // Base URI is used with relative requests
@@ -81,8 +80,8 @@ class Persona {
     }
 
     public function actualizarPersona($id, $carnet, $complemento, $fechanacimiento, $paterno, $materno, $nombre, $genero) {
-        /***NUEVO TOKEN A LA FECHA 25/09/2017*/
-        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU0MDQ5MTY3OSwiZXhwIjoxNTcyMDI3Njc5fQ.lf233gXhht6PCcs29WmKN9a2Sy-sj_buKtJVsbM_CPc';
+        /***NUEVO TOKEN A LA FECHA 28/10/2019*/
+        $token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MDYxNjQsImlhdCI6MTU3MjI3MzAzMiwiZXhwIjoxNjAzODA5MDMyfQ.F4Vu2jxfQzuZDWWR5wD93aRNTKKTfMYTVE2m4Wu8GIQ';
 
         $client = new Client([
         // Base URI is used with relative requests
