e086cff39c2f3abfbd2b97cf5dd5cec2a9a090c6
krlos <pckrlos@gmail.com>
2020-01-24 18:30:05 -0400

    mapped ControlInstalador table -siged

--- src/Sie/AppWebBundle/Entity/ControlInstalador.php
+++ src/Sie/AppWebBundle/Entity/ControlInstalador.php
@@ -164,4 +164,88 @@ class ControlInstalador
     {
         return $this->gestionTipoId;
     }
+    /**
+     * @var string
+     */
+    private $detalle;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Set detalle
+     *
+     * @param string $detalle
+     * @return ControlInstalador
+     */
+    public function setDetalle($detalle)
+    {
+        $this->detalle = $detalle;
+    
+        return $this;
+    }
+
+    /**
+     * Get detalle
+     *
+     * @return string 
+     */
+    public function getDetalle()
+    {
+        return $this->detalle;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return ControlInstalador
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return ControlInstalador
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
 }
