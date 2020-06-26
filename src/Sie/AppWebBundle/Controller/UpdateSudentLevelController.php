00f615bd99508ca81e092e8ee29aab1a58fb7f5b
krlos <pckrlos@gmail.com>
2020-06-26 11:26:44 -0400

    added new functionality in Ini-1,2, - pri-1  on udpate level-grado option - siged

--- src/Sie/AppWebBundle/Controller/UpdateSudentLevelController.php
+++ src/Sie/AppWebBundle/Controller/UpdateSudentLevelController.php
@@ -63,11 +63,13 @@ class UpdateSudentLevelController extends Controller{
         $message = "";
         $status = "";
         $existStudentData = false;
+        $swUpdateLevelGradoIniPri = false;
 
         $arrCurrenteInscription = array();
         $arrLastInscription = array();
         $arrNextLevel = array();
         $arrParalelos = array();
+        $arrLevel = array();
         $swObservation = false;
         $messageObservaation = '';
         
@@ -108,6 +110,26 @@ class UpdateSudentLevelController extends Controller{
               next($inscriptions);
             }
 
+             $arrayConditionInscription = array(
+                    'codigoRude'=>$codigoRude,
+                    'matriculaId'=>4,
+                    'gestion'=>$this->currentyear,
+                );
+            $arrCurrenteInscription = $this->get('funciones')->getCurrentInscriptionByRudeAndGestionAndMatricula($arrayConditionInscription);
+            
+            
+            if(sizeof($arrCurrenteInscription)>0){
+                $arrCurrenteInscription = $arrCurrenteInscription[0];
+                $arrNextLevel['studenInscriptionId']=$arrCurrenteInscription['studenInscriptionId'];
+                $arrNextLevel['sie']=$arrCurrenteInscription['sie'];
+                $arrNextLevel['gestion']=$arrCurrenteInscription['gestion'];
+                $arrNextLevel['codigoRude']=$codigoRude;
+                $arrNextLevel['oldDataInscription']=json_encode($arrCurrenteInscription) ;
+
+            }else{
+                $arrCurrenteInscription = array();
+            }
+
             // thee student has history_?
             if(!$sw){
                 $arrayConditionInscription = array(
@@ -116,48 +138,63 @@ class UpdateSudentLevelController extends Controller{
                     'gestion'=>$this->currentyear,
                 );
                 $arrCurrenteInscription = $this->get('funciones')->getCurrentInscriptionByRudeAndGestionAndMatricula($arrayConditionInscription);
-                $arrCurrenteInscription = $arrCurrenteInscription[0];
+                if(sizeof($arrCurrenteInscription)>0){
 
-                $arrNextLevel = $this->getInfoInscriptionStudent($arrLastInscription['nivelId'].$arrLastInscription['cicloId'].$arrLastInscription['gradoId'],$arrLastInscription['estadoMatriculaId']);
+                    $arrCurrenteInscription = $arrCurrenteInscription[0];
 
-                if( $arrCurrenteInscription['nivelId']==$arrNextLevel['nivelId'] && $arrCurrenteInscription['cicloId']==$arrNextLevel['cicloId'] && $arrCurrenteInscription['gradoId']==$arrNextLevel['gradoId']){
-                    $swObservation = false;
-                    $messageObservaation = 'No presenta Observación';
-                    
-                }else{
-                    $swObservation = true;
-                    $messageObservaation = 'Presenta Observación';
-                    
-                    $objInfoCurso = $em->getRepository('SieAppWebBundle:InstitucioneducativaCurso')->findBy(array(
-                        'nivelTipo'=>$arrNextLevel['nivelId'],
-                        'cicloTipo'=>$arrNextLevel['cicloId'],
-                        'gradoTipo'=>$arrNextLevel['gradoId'],
-                        'institucioneducativa'=>$arrCurrenteInscription['sie'],
-                        'gestionTipo'=>$this->currentyear,
-                    ));
-                    $arrParalelos = array();
-                    foreach ($objInfoCurso as  $value) {
-                        $arrParalelos[] = array('id'=>$value->getParaleloTipo()->getId(),'paralelo'=>$em->getRepository('SieAppWebBundle:ParaleloTipo')->find($value->getParaleloTipo()->getId())->getParalelo() );
+                    $arrNextLevel = $this->getInfoInscriptionStudent($arrLastInscription['nivelId'].$arrLastInscription['cicloId'].$arrLastInscription['gradoId'],$arrLastInscription['estadoMatriculaId']);
+
+                    if( $arrCurrenteInscription['nivelId']==$arrNextLevel['nivelId'] && $arrCurrenteInscription['cicloId']==$arrNextLevel['cicloId'] && $arrCurrenteInscription['gradoId']==$arrNextLevel['gradoId']){
+                        $swObservation = false;
+                        $messageObservaation = 'No presenta Observación';
+                        
+                    }else{
+                        $swObservation = true;
+                        $messageObservaation = 'Presenta Observación';
+                        
+                        $objInfoCurso = $em->getRepository('SieAppWebBundle:InstitucioneducativaCurso')->findBy(array(
+                            'nivelTipo'=>$arrNextLevel['nivelId'],
+                            'cicloTipo'=>$arrNextLevel['cicloId'],
+                            'gradoTipo'=>$arrNextLevel['gradoId'],
+                            'institucioneducativa'=>$arrCurrenteInscription['sie'],
+                            'gestionTipo'=>$this->currentyear,
+                        ));
+                        $arrParalelos = array();
+                        foreach ($objInfoCurso as  $value) {
+                            $arrParalelos[] = array('id'=>$value->getParaleloTipo()->getId(),'paralelo'=>$em->getRepository('SieAppWebBundle:ParaleloTipo')->find($value->getParaleloTipo()->getId())->getParalelo() );
+                        }
+
+                        $arrNextLevel['nivel']=$em->getRepository('SieAppWebBundle:NivelTipo')->find($arrNextLevel['nivelId'])->getNivel() ;
+                        $arrNextLevel['grado']=$em->getRepository('SieAppWebBundle:GradoTipo')->find($arrNextLevel['gradoId'])->getGrado() ;
+                        
                     }
 
-                    $arrNextLevel['studenInscriptionId']=$arrCurrenteInscription['studenInscriptionId'];
-                    $arrNextLevel['sie']=$arrCurrenteInscription['sie'];
-                    $arrNextLevel['gestion']=$arrCurrenteInscription['gestion'];
-                    $arrNextLevel['codigoRude']=$codigoRude;
-                    $arrNextLevel['nivel']=$em->getRepository('SieAppWebBundle:NivelTipo')->find($arrNextLevel['nivelId'])->getNivel() ;
-                    $arrNextLevel['grado']=$em->getRepository('SieAppWebBundle:GradoTipo')->find($arrNextLevel['gradoId'])->getGrado() ;
-                    $arrNextLevel['oldDataInscription']=json_encode($arrCurrenteInscription) ;
-                    
+                    $existStudentData = true;                                    
+
+                }else{
+
+                    $swObservation = false;
+                    $messageObservaation = 'No presenta Observación';
+                    $existStudentData = true; 
 
                 }
-
-             
-                $existStudentData = true;                
+                
 
             }else{
+                
+                $levelAllow = array(111,112,121);
+                $levelAndGrado = $arrCurrenteInscription['nivelId'].$arrCurrenteInscription['gradoId'];
+                if( in_array($levelAndGrado, $levelAllow) ){
+                    // dump($arrNextLevel);die;
+                    $swUpdateLevelGradoIniPri = true;  
+                    $arrLevel = $this->getInfoUe($arrCurrenteInscription['sie']);
+                    // $arrNextLevel['nivel']=$em->getRepository('SieAppWebBundle:NivelTipo')->find(1)->getNivel() ;
+                    // $arrNextLevel['grado']=$em->getRepository('SieAppWebBundle:GradoTipo')->find(2)->getGrado() ;  
+                }
                 $swObservation = false;
                 $messageObservaation = 'No presenta Observación';
                 $existStudentData = true; 
+
             }
 
 
@@ -165,7 +202,7 @@ class UpdateSudentLevelController extends Controller{
         }else{
             // the studnet no exist
             $code = 200;
-            $message = "Estudiante No existe";
+            $message = "Estudiante No existeo no cuenta con Historial";
             $status = "";
             $existStudentData = false;
             $swObservation = false;
@@ -175,23 +212,26 @@ class UpdateSudentLevelController extends Controller{
         'status'          => $status,
         'code'            => $code,
         'message'         => $message,
+        'arrNextLevel' => $arrNextLevel,
+        'arrParalelos' => $arrParalelos,
+        'swObservation' => $swObservation,
         'dataInscriptionR' => $inscriptions,    
         'arrStudentExist' => $arrStudentExist,
-        'existStudentData' => $existStudentData,
-        'swObservation' => $swObservation,
+        'existStudentData' => $existStudentData,        
         'messageObservaation' => $messageObservaation,
-        'arrParalelos' => $arrParalelos,
         'arrCurrenteInscription' => $arrCurrenteInscription,
-        'arrNextLevel' => $arrNextLevel,
-
-
-        
+        'swUpdateLevelGradoIniPri' => $swUpdateLevelGradoIniPri,        
+        'arrLevel' => $arrLevel,        
       );
       
       $response->setStatusCode(200);
       $response->setData($arrResponse);
 
       return $response;
+    }
+
+    private function getCurrentInscritpion($data){
+
     }
 
     /**
@@ -318,7 +358,7 @@ class UpdateSudentLevelController extends Controller{
     }
 
     public function doUpdateAction(Request $request){
-
+        // dump($request);die;
         // ini vars
         $response = new JsonResponse();
         $em = $this->getDoctrine()->getManager();
@@ -326,7 +366,7 @@ class UpdateSudentLevelController extends Controller{
         // get the send values
         $codigoRude = $request->get('codigoRude');
         $nivelId = $request->get('nivelId');
-        $cicloId = $request->get('cicloId');
+        // $cicloId = $request->get('cicloId');
         $gradoId = $request->get('gradoId');
         $paraleloId = $request->get('paraleloId');
         $turnoId = $request->get('turnoId');
@@ -336,7 +376,7 @@ class UpdateSudentLevelController extends Controller{
         // condition to find the correct level to fix the observation
         $arrayCondition = array(
             'nivelTipo' => $nivelId,
-            'cicloTipo' => $cicloId,
+            // 'cicloTipo' => $cicloId,
             'gradoTipo' => $gradoId,
             'paraleloTipo' => $paraleloId,
             'turnoTipo' => $turnoId,
@@ -391,6 +431,220 @@ class UpdateSudentLevelController extends Controller{
 
           return $response;
 
-    }       
+    }  
+
+    /**
+     * get the levels by sie and year
+     * @param type $id like sie
+     * @param type $n like nivel
+     * @param type $g like grado
+     * @return type
+     */
+    private function getInfoUe($id) {
+        
+        $em = $this->getDoctrine()->getManager();
+
+        // $id = $request->get('sie');
+        //get the tuicion
+        //select * from get_ue_tuicion(137746,82480002)
+        /*
+          $query = $em->getConnection()->prepare('SELECT get_ue_tuicion (:user_id::INT, :sie::INT)');
+          $query->bindValue(':user_id', $this->session->get('userId'));
+          $query->bindValue(':sie', $id);
+          $query->execute();
+          $aTuicion = $query->fetchAll();
+     */
+    
+            //get the tuicion
+            $query = $em->getConnection()->prepare('SELECT get_ue_tuicion (:user_id::INT, :sie::INT, :roluser::INT)');
+            $query->bindValue(':user_id', $this->session->get('userId'));
+            $query->bindValue(':sie', $id);
+            $query->bindValue(':roluser', $this->session->get('roluser'));
+            $query->execute();
+            $aTuicion = $query->fetchAll();
+
+        $aniveles = array();
+         if ($aTuicion[0]['get_ue_tuicion']) {
+        //get the IE
+        $institucion = $em->getRepository('SieAppWebBundle:Institucioneducativa')->find($id);
+        if($institucion){
+            
+            $em = $this->getDoctrine()->getManager();
+            //get the Niveles
+
+            $entity = $em->getRepository('SieAppWebBundle:InstitucioneducativaCurso');
+            $query = $entity->createQueryBuilder('iec')
+                    ->select('(iec.nivelTipo)')
+                    //->leftjoin('SieAppWebBundle:InstitucioneducativaCurso', 'iec', 'WITH', 'ei.institucioneducativaCurso = iec.id')
+                    ->where('iec.institucioneducativa = :sie')
+                    ->andwhere('iec.gestionTipo = :gestion')
+                    ->andwhere('iec.nivelTipo != :nivel')
+                    ->setParameter('sie', $id)
+                    ->setParameter('gestion', $this->session->get('currentyear') )
+                    ->setParameter('nivel', '13')
+                    ->orderBy('iec.nivelTipo', 'ASC')
+                    ->distinct()
+                    ->getQuery();
+            $aNiveles = $query->getResult();
+            if($aNiveles){
+                $aniveles = array();
+                foreach ($aNiveles as $nivel) {
+                    $aniveles[] = array('id'=> $nivel[1], 'level'=>$em->getRepository('SieAppWebBundle:NivelTipo')->find($nivel[1])->getNivel());
+                }
+                $status = 'success';
+                $code = 200;
+                $message = "Datos encontrados";
+                $swprocess = true; 
+                $nombreIE = ($institucion) ? $institucion->getInstitucioneducativa() : "";
+            }else{
+                $status = 'error';
+                $code = 400;
+                $message = "Información no consolidada - no tiene level";
+                $swprocess = false; 
+                $nombreIE = false; 
+            }
+            
+
+        }else{
+
+            $status = 'error';
+            $code = 400;
+            $message = "Unidad Educativa no existe";
+            $swprocess = false; 
+            $nombreIE = false; 
+
+        }
+        
+        
+      } else {
+          $message = 'Usuario No tiene Tuición';
+           $status = 'error';
+               $code = 400;
+               //$message = "Unidad Educativa no existe";
+               $swprocess = false;
+               $nombreIE = false;
+
+      } 
+
+      return $aniveles;
+
+      // $arrResponse = array(
+      //   'status'          => $status,
+      //   'code'            => $code,
+      //   'message'         => $message,
+      //   'swprocessue' => $swprocess,    
+      //   'nombreue' => $nombreIE, 
+      //   'aniveles' => $aniveles
+      //   );
+      
+      // $response->setStatusCode(200);
+      // $response->setData($arrResponse);
+
+      // return $response;
+
+        
+      //   $response->setStatusCode(200);
+      //   return $response->setData(array());
+    }  
+    
+    /**
+     * get grado
+     * @param type $idnivel
+     * @param type $sie
+     * return list of grados
+     */
+    public function setGradoAction(Request  $request) {
+        // ini vars
+        $em = $this->getDoctrine()->getManager();
+        $response = new JsonResponse();
+        
+        // get the send values
+        $sie = $request->get('sie');
+        $idnivel = $request->get('nivelId');
+        $gestionselected = $request->get('gestion');;
+
+        //get grado
+        $agrados = array();
+        $entity = $em->getRepository('SieAppWebBundle:InstitucioneducativaCurso');
+        $query = $entity->createQueryBuilder('iec')
+                ->select('(iec.gradoTipo)')
+                //->leftjoin('SieAppWebBundle:InstitucioneducativaCurso', 'iec', 'WITH', 'ei.institucioneducativaCurso = iec.id')
+                ->where('iec.institucioneducativa = :sie')
+                ->andWhere('iec.nivelTipo = :idnivel')
+                ->andwhere('iec.gestionTipo = :gestion')
+                ->setParameter('sie', $sie)
+                ->setParameter('idnivel', $idnivel)
+                ->setParameter('gestion', $gestionselected)
+                ->distinct()
+                ->orderBy('iec.gradoTipo', 'ASC')
+                ->getQuery();
+        $aGrados = $query->getResult();
+
+        $agrados = array();
+        
+        foreach ($aGrados as $grado) {
+            if($idnivel == 12 && $grado[1]==1){
+                $agrados[$grado[1]] = array('id'=>$grado[1], 'grado'=>$em->getRepository('SieAppWebBundle:GradoTipo')->find($grado[1])->getGrado() );
+            }
+            if($idnivel == 11){
+                $agrados[$grado[1]] = array('id'=>$grado[1], 'grado'=>$em->getRepository('SieAppWebBundle:GradoTipo')->find($grado[1])->getGrado() );
+            }            
+        }
+      $arrResponse = array(
+        'arrGrado' => $agrados
+      );
+      $response->setStatusCode(200);
+      $response->setData($arrResponse);
+
+      return $response;
+        
+    }
+
+    /**
+     * get the paralelos
+     * @param type $idnivel
+     * @param type $sie
+     * @return type
+     */
+    public function setParaleloAction(Request $request) {
+        // ini vars
+        $em = $this->getDoctrine()->getManager();
+        $response = new JsonResponse();
+        // get the send values
+        $sie = $request->get('sie');
+        $nivel = $request->get('nivelId');
+        $grado = $request->get('gradoId');
+        $gestion = $request->get('gestion');;
+        //get paralelo
+        $aparalelos = array();
+        $entity = $em->getRepository('SieAppWebBundle:InstitucioneducativaCurso');
+        $query = $entity->createQueryBuilder('iec')
+                ->select('(iec.paraleloTipo)')
+                //->leftjoin('SieAppWebBundle:InstitucioneducativaCurso', 'iec', 'WITH', 'ei.institucioneducativaCurso = iec.id')
+                ->where('iec.institucioneducativa = :sie')
+                ->andWhere('iec.nivelTipo = :nivel')
+                ->andwhere('iec.gradoTipo = :grado')
+                ->andwhere('iec.gestionTipo = :gestion')
+                ->setParameter('sie', $sie)
+                ->setParameter('nivel', $nivel)
+                ->setParameter('grado', $grado)
+                ->setParameter('gestion', $gestion)
+                ->distinct()
+                ->orderBy('iec.paraleloTipo', 'ASC')
+                ->getQuery();
+        $aParalelos = $query->getResult();
+        $aparalelos = array();
+        foreach ($aParalelos as $paralelo) {
+            $aparalelos[$paralelo[1]] = array('id'=>$paralelo[1], 'paralelo'=>$em->getRepository('SieAppWebBundle:ParaleloTipo')->find($paralelo[1])->getParalelo() );    
+        }
+
+      $arrResponse = array(
+        'arrParalelo' => $aparalelos
+      );
+      $response->setStatusCode(200);
+      $response->setData($arrResponse);
+
+      return $response;
+    }              
 
 }
