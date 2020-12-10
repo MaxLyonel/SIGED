a5bcf852de38bd7eb67716a3e29fe887bfd73d7b
Carlos Pacha Cordova <pckrlos@gmail.com>
2020-12-09 20:40:43 -0400

    dev student acreditation option added model data - siged

---
+++ src/Sie/AppWebBundle/Entity/EstudianteInscripcionAcreditacion.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EstudianteInscripcionAcreditacion
+ */
+class EstudianteInscripcionAcreditacion
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $observacion;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaRegistro;
+
+    /**
+     * @var string
+     */
+    private $evento;
+
+    /**
+     * @var \Sie\AppWebBundle\Entity\Estudiante
+     */
+    private $estudiante;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set observacion
+     *
+     * @param string $observacion
+     * @return EstudianteInscripcionAcreditacion
+     */
+    public function setObservacion($observacion)
+    {
+        $this->observacion = $observacion;
+    
+        return $this;
+    }
+
+    /**
+     * Get observacion
+     *
+     * @return string 
+     */
+    public function getObservacion()
+    {
+        return $this->observacion;
+    }
+
+    /**
+     * Set fechaRegistro
+     *
+     * @param \DateTime $fechaRegistro
+     * @return EstudianteInscripcionAcreditacion
+     */
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaRegistro
+     *
+     * @return \DateTime 
+     */
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+
+    /**
+     * Set evento
+     *
+     * @param string $evento
+     * @return EstudianteInscripcionAcreditacion
+     */
+    public function setEvento($evento)
+    {
+        $this->evento = $evento;
+    
+        return $this;
+    }
+
+    /**
+     * Get evento
+     *
+     * @return string 
+     */
+    public function getEvento()
+    {
+        return $this->evento;
+    }
+
+    /**
+     * Set estudiante
+     *
+     * @param \Sie\AppWebBundle\Entity\Estudiante $estudiante
+     * @return EstudianteInscripcionAcreditacion
+     */
+    public function setEstudiante(\Sie\AppWebBundle\Entity\Estudiante $estudiante = null)
+    {
+        $this->estudiante = $estudiante;
+    
+        return $this;
+    }
+
+    /**
+     * Get estudiante
+     *
+     * @return \Sie\AppWebBundle\Entity\Estudiante 
+     */
+    public function getEstudiante()
+    {
+        return $this->estudiante;
+    }
+}
