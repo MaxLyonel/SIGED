6e54b815a32515bbc0ef66644d0f50c25fc196bf
Carlos Pacha Cordova <pckrlos@gmail.com>
2021-04-01 14:55:44 -0400

    enables traslado option to the Tarija Depto user - academico

--- src/Sie/RegularBundle/Controller/InscriptionIniPriTrueController.php
+++ src/Sie/RegularBundle/Controller/InscriptionIniPriTrueController.php
@@ -41,7 +41,11 @@ class InscriptionIniPriTrueController extends Controller {
      *
      */
     public function indexAction() {
+      //enable to departamento user
+      if($this->session->get('userName')!='1897494'){
         return $this->redirect($this->generateUrl('login'));
+      }
+      
       /*
       $arrUser = array('6478217','7861595','3558624');
 
