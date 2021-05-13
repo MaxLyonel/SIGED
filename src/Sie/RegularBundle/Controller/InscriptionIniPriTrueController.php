3cbd18796e079dba40f4ede5074b395f4b61ae3b
krlos <pckrlos@gmail.com>
2021-05-13 11:45:21 -0400

    enabled modadlidad, multigrado, traslado option - academico

--- src/Sie/RegularBundle/Controller/InscriptionIniPriTrueController.php
+++ src/Sie/RegularBundle/Controller/InscriptionIniPriTrueController.php
@@ -42,7 +42,7 @@ class InscriptionIniPriTrueController extends Controller {
      */
     public function indexAction() {
       //disabled option by krlos
-     if (in_array($this->session->get('roluser'), array(8,7))){
+     if (in_array($this->session->get('roluser'), array(8,7,10))){
 
      }else{
       return $this->redirect($this->generateUrl('login'));  
