9273d7a3105dc6767b696142572e5fb35a8b93e5
krlos <pckrlos@gmail.com>
2021-11-22 20:56:06 -0400

    disabled IDB options - acadmico/alternativa

--- src/Sie/AppWebBundle/Controller/BachillerExcelenciaController.php
+++ src/Sie/AppWebBundle/Controller/BachillerExcelenciaController.php
@@ -29,7 +29,7 @@ class BachillerExcelenciaController extends Controller {
     public function __construct() {
         $this->session = new Session();
         $this->fechaActual = new \DateTime('now');
-        $this->fechaCorte = new \DateTime('2021-11-23');
+        $this->fechaCorte = new \DateTime('2020-11-23');
         $this->gestionOperativo =  $this->session->get('currentyear');
     }
 
