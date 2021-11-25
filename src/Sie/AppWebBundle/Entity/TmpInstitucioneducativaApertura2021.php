87fb139be8d7afeb614b5facc8b944aabe7332d5
krlos <pckrlos@gmail.com>
2021-11-25 15:38:23 -0400

    added 1er trim unable option - academico

---
+++ src/Sie/AppWebBundle/Entity/TmpInstitucioneducativaApertura2021.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sie\AppWebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TmpInstitucioneducativaApertura2021
+ */
+class TmpInstitucioneducativaApertura2021
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $institucioneducativaId;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaRegistro;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set institucioneducativaId
+     *
+     * @param integer $institucioneducativaId
+     * @return TmpInstitucioneducativaApertura2021
+     */
+    public function setInstitucioneducativaId($institucioneducativaId)
+    {
+        $this->institucioneducativaId = $institucioneducativaId;
+    
+        return $this;
+    }
+
+    /**
+     * Get institucioneducativaId
+     *
+     * @return integer 
+     */
+    public function getInstitucioneducativaId()
+    {
+        return $this->institucioneducativaId;
+    }
+
+    /**
+     * Set fechaRegistro
+     *
+     * @param \DateTime $fechaRegistro
+     * @return TmpInstitucioneducativaApertura2021
+     */
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+    
+        return $this;
+    }
+
+    /**
+     * Get fechaRegistro
+     *
+     * @return \DateTime 
+     */
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+}
