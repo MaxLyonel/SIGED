1bc45a4bca8018b6907b26693e6cc395e970a4c7
Juan Fernando <jchambi@minedu.gob.bo>
2021-11-30 18:50:05 -0400

    retirar el estado de '9'=>'RETIRADO TRASLADO' del array  cambio de estado

--- src/Sie/RegularBundle/Controller/RemoveInscriptionStudentFreeController.php
+++ src/Sie/RegularBundle/Controller/RemoveInscriptionStudentFreeController.php
@@ -202,11 +202,13 @@ class RemoveInscriptionStudentFreeController extends Controller {
       // $arrEstados = array('4'=>'Efectivo', '10'=>'Abandono');
       $rolesAllowed = array(7,10);
       if(in_array($rolUser,$rolesAllowed)){
-        $arrEstados = array('4'=>'EFECTIVO', '9'=>'RETIRADO TRASLADO','5'=>'PROMOVIDO');
+        // $arrEstados = array('4'=>'EFECTIVO', '9'=>'RETIRADO TRASLADO','5'=>'PROMOVIDO');
+        $arrEstados = array('4'=>'EFECTIVO', '5'=>'PROMOVIDO');
       }else{
         // $arrEstados = array( '10'=>'RETIRO ABANDONO',/*'6'=>'NO INCORPORADO','9'=>'RETIRADO TRASLADO'*/);
         if($rolUser == 8)
-          $arrEstados = array('4'=>'EFECTIVO', '10'=>'RETIRO ABANDONO','6'=>'NO INCORPORADO','9'=>'RETIRADO TRASLADO','5'=>'PROMOVIDO');
+          // $arrEstados = array('4'=>'EFECTIVO', '10'=>'RETIRO ABANDONO','6'=>'NO INCORPORADO','9'=>'RETIRADO TRASLADO','5'=>'PROMOVIDO');
+          $arrEstados = array('4'=>'EFECTIVO', '10'=>'RETIRO ABANDONO','6'=>'NO INCORPORADO','5'=>'PROMOVIDO');
         else
           $arrEstados = array();
       }
