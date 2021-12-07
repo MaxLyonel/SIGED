06978a8e02c5cb2121b676cf2d859953f26fbd8a
Juan Fernando <jchambi@minedu.gob.bo>
2021-12-07 09:19:12 -0400

    ajuste de habilitacion de estado 10 estadomatricula_tipo_id_fin_r

--- src/Sie/HerramientaBundle/Controller/OperativoBonoJPController.php
+++ src/Sie/HerramientaBundle/Controller/OperativoBonoJPController.php
@@ -364,15 +364,23 @@ class OperativoBonoJPController extends Controller
          //check if the student has current inscription
         if(sizeof($currentInscription)>0){
          	// if the student is in the same UE
-         	if( ($this->session->get('roluser') == '7') || ($this->session->get('roluser') == '8')  || ($this->session->get('roluser') == '9')) {
-         		$messageError = 'roles...';
+         	if($currentInscription[0]['institucioneducativa_id']!=$this->session->get('ie_id')){
+         		if( ($this->session->get('roluser') == '7') || ($this->session->get('roluser') == '8')  || ($this->session->get('roluser') == '9')) {
+         			$swError = false;
+         		}else{
+	         		$messageError = 'El estudiante no esta inscrito en esta UE';
+	         		$swError = true;
+         		}
+         	}  
+         	/*if( ($this->session->get('roluser') == '7') || ($this->session->get('roluser') == '8')  || ($this->session->get('roluser') == '9')) {
+         		// $messageError = 'roles...';
          		$swError = false;
          	}else{
 	         	if($currentInscription[0]['institucioneducativa_id']!=$this->session->get('ie_id')){
 	         		$messageError = 'El estudiante no esta inscrito en esta UE';
 	         		$swError = true;
 	         	}         		
-         	}
+         	}*/
         }else{
          	$messageError = 'El estudiante no cuenta con inscription';
          	$swError = true;
@@ -388,7 +396,7 @@ class OperativoBonoJPController extends Controller
 			$em = $this->getDoctrine()->getManager();
 			if($idtipoInstitucion == 1)
 			{
-				$query = $em->getConnection()->prepare("SELECT  * FROM sp_genera_estudiante_historial(?) where gestion_tipo_id_raep = ? AND estadomatricula_tipo_id_fin_r  IN (4,5,55,11) ");
+				$query = $em->getConnection()->prepare("SELECT  * FROM sp_genera_estudiante_historial(?) where gestion_tipo_id_raep = ? AND estadomatricula_tipo_id_fin_r  IN (4,5,55,11,10) ");
 				// $params = array($codigo_rude, $gestion, $estado_matricula);
 				$params = array($codigo_rude, $gestion);
 
