eb8b56cbe2247822ea1f19e69de63cd166b5da74
cristinavallejos <cvallejos@minedu.gob.bo>
2022-01-25 13:22:05 -0400

    ajuste restablecer Operativo Especial

--- src/Sie/EspecialBundle/Controller/InfoCentroController.php
+++ src/Sie/EspecialBundle/Controller/InfoCentroController.php
@@ -419,6 +419,7 @@ class InfoCentroController extends Controller {
                 $gestionesArray[$value->getId()] = $value->getGestion();
             }
         }
+        
         return $this->render($this->session->get('pathSystem') . ':Institucioneducativa:index_gestion.html.twig', array(
             'consol' => $consol,
             'gestiones' => $gestionesArray,
