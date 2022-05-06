5055c071b22aa6e61c021a9df0ba8e67b0a4fc00
Iver Guaygua <iverosvaldo@gmail.com>
2022-05-06 08:42:28 -0400

    ACADEMICO - incorporacion de datos ci whenayek

--- src/Sie/RegularBundle/Controller/ModCiLocalidadController.php
+++ src/Sie/RegularBundle/Controller/ModCiLocalidadController.php
@@ -250,10 +250,19 @@ class ModCiLocalidadController extends Controller {
             } else {
                 $formStudent->add('nombre', 'text', array('label' => 'Nombre', 'data' => $data['nombre'], 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)));
             }
+
+            if ($data['segipId']==0) {
+                $formStudent->add('ci', 'text', array('label' => 'CI', 'data' => $data['carnetIdentidad'], 'required' => true, 'attr' => array('class' => 'form-control', 'pattern' => '[0-9]{3,10}', 'maxlength' => 10, 'style' => 'text-transform:uppercase', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Si el Carnet de Identidad es extranjero, debe omitir la parte 'E-', pero debe escribir los ceros que contenga el C.I.")));
+                $formStudent->add('complemento', 'text', array('label' => 'Complemento', 'data' => $data['complemento'], 'required' => false, 'attr' => array('maxlength' => 2, 'pattern' => '[0-9a-zA-Z]{2}', 'style' => 'text-transform:uppercase', 'class' => 'form-control', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Complemento no es lo mismo que la expedición del C.I. Por favor NO coloque abreviaturas de Departamentos")));
+            } else {
+                $formStudent->add('ci', 'text', array('label' => 'CI', 'data' => $data['carnetIdentidad'], 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)));
+                $formStudent->add('complemento', 'text', array('label' => 'Complemento', 'data' => $data['complemento'], 'required' => false, 'attr' => array('maxlength' => 2, 'pattern' => '[0-9a-zA-Z]{2}', 'style' => 'text-transform:uppercase', 'class' => 'form-control', 'disabled' => true)));
+                //$formStudent->add('nombre', 'text', array('label' => 'Nombre', 'data' => $data['nombre'], 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)));
+            }
             $generos = [1,2];
             $formStudent
-            //->add('genero', 'text', array('label' => 'Género', 'data' => $data['genero'], 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)))
-             ->add('genero', 'entity', array(
+            ->add('genero', 'text', array('label' => 'Género', 'data' => $data['genero'], 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)))
+            /* ->add('genero', 'entity', array(
              'class' => 'SieAppWebBundle:GeneroTipo',
              'query_builder' => function (EntityRepository $e) use ($generos)
              {
@@ -266,13 +275,13 @@ class ModCiLocalidadController extends Controller {
              'required'=>true,
              'attr' => array('class' => 'form-control')
              //'data'=>($data['genero'] != null)?$em->getReference('SieAppWebBundle:GeneroTipo', $data['genero']):''
-             ))
+             ))*/
             ->add('pais', 'text', array('label' => 'Pais', 'data' => strtoupper($data['pais']), 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)))
             ->add('departamento', 'text', array('label' => 'Departamento', 'data' => strtoupper($data['departamento']), 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)))
             ->add('provincia', 'text', array('label' => 'Provincia', 'data' => strtoupper($data['provincia']), 'required' => false, 'attr' => array('class' => 'form-control', 'disabled' => true)))
             ->add('localidad', 'text', array('label' => 'Localidad', 'data' => strtoupper($data['localidadNac']), 'attr' => array('class' => 'form-control', 'disabled' => true ,'style' => 'text-transform:uppercase', 'maxlength' => 50)))
-            ->add('ci', 'text', array('label' => 'CI', 'data' => $data['carnetIdentidad'], 'required' => true, 'attr' => array('class' => 'form-control', 'disabled' => true , 'pattern' => '[0-9]{3,10}', 'maxlength' => 10, 'style' => 'text-transform:uppercase', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Si el Carnet de Identidad es extranjero, debe omitir la parte 'E-', pero debe escribir los ceros que contenga el C.I.")))
-            ->add('complemento', 'text', array('label' => 'Complemento', 'data' => $data['complemento'], 'required' => false, 'attr' => array('maxlength' => 2, 'disabled' => true, 'pattern' => '[0-9a-zA-Z]{2}', 'style' => 'text-transform:uppercase', 'class' => 'form-control', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Complemento no es lo mismo que la expedición del C.I. Por favor NO coloque abreviaturas de Departamentos")))
+            //->add('ci', 'text', array('label' => 'CI', 'data' => $data['carnetIdentidad'], 'required' => true, 'attr' => array('class' => 'form-control', 'disabled' => true , 'pattern' => '[0-9]{3,10}', 'maxlength' => 10, 'style' => 'text-transform:uppercase', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Si el Carnet de Identidad es extranjero, debe omitir la parte 'E-', pero debe escribir los ceros que contenga el C.I.")))
+            //->add('complemento', 'text', array('label' => 'Complemento', 'data' => $data['complemento'], 'required' => false, 'attr' => array('maxlength' => 2, 'disabled' => true, 'pattern' => '[0-9a-zA-Z]{2}', 'style' => 'text-transform:uppercase', 'class' => 'form-control', 'data-toggle' => "tooltip", 'data-placement' => "right", 'data-original-title' => "Complemento no es lo mismo que la expedición del C.I. Por favor NO coloque abreviaturas de Departamentos")))
             ->add('fechaNacimiento', 'date', array('widget' => 'single_text', 'format' => 'dd-MM-yyyy', 'label' => 'Fecha de Nacimiento', 'data' => $data['fechaNacimiento'], 'required' => false, 'attr' => array('class' => 'form-control calendario','disabled' => true)));
         
         $formStudent->add('save', 'submit', array('label' => 'Guardar cambios'));
