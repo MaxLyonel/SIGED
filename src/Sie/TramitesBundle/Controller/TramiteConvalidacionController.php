17b73d71be7eb1f90cc2c43ebda93401c32691e5
roly.canaviri <jurlan.roly@gmail.com>
2022-05-18 09:54:21 -0400

    incorporacion de estados promovido por postbachillerado en convalidaciones diplomas

--- src/Sie/TramitesBundle/Controller/TramiteConvalidacionController.php
+++ src/Sie/TramitesBundle/Controller/TramiteConvalidacionController.php
@@ -190,7 +190,7 @@ class TramiteConvalidacionController extends Controller {
             inner join estudiante_inscripcion as ei on ei.estudiante_id = e.id
             inner join institucioneducativa_curso as iec on iec.id = ei.institucioneducativa_curso_id
             inner join institucioneducativa as ie on ie.id = iec.institucioneducativa_id
-            where iec.nivel_tipo_id in (13,3) and ei.estadomatricula_tipo_id in (4,5,55) and e.id = :id
+            where iec.nivel_tipo_id in (13,3) and ei.estadomatricula_tipo_id in (4,5,55,100,102) and e.id = :id
             union all
             select distinct sat.codigo as nivel_tipo_id, ies.gestion_tipo_id, sest.codigo as grado_tipo_id, ie.id as institucioneducativa_id, ie.institucioneducativa
             from superior_facultad_area_tipo as sfat
@@ -204,7 +204,7 @@ class TramiteConvalidacionController extends Controller {
             inner join estudiante_inscripcion as ei on iec.id=ei.institucioneducativa_curso_id
             inner join (select * from estudiante where id = :id) as e on ei.estudiante_id=e.id
             inner join institucioneducativa as ie on ie.id = siea.institucioneducativa_id
-            where  sfat.codigo in (15) and sat.codigo in (2,3) and sest.codigo in (2) and ei.estadomatricula_tipo_id in (4,5,55)  
+            where  sfat.codigo in (15) and sat.codigo in (2,3) and sest.codigo in (2) and ei.estadomatricula_tipo_id in (4,5,55,100,102)  
             order by gestion_tipo_id, nivel_tipo_id, grado_tipo_id
         ");
         $query->bindValue(':id', $id);
@@ -882,7 +882,7 @@ class TramiteConvalidacionController extends Controller {
                 inner join estudiante_inscripcion as ei on ei.estudiante_id = e.id
                 inner join institucioneducativa_curso as iec on iec.id = ei.institucioneducativa_curso_id
                 where case :ci::varchar when '' then e.pasaporte = :pasaporte when '0' then e.pasaporte = :pasaporte else e.carnet_identidad = :ci::varchar and e.complemento = :complemento::varchar end and iec.gestion_tipo_id = :gestion::int and iec.nivel_tipo_id = :nivel::int and iec.grado_tipo_id = :grado::int     
-                and ei.estadomatricula_tipo_id in (4,5,55,11)
+                and ei.estadomatricula_tipo_id in (4,5,55,11,100,102)
                 ");
         $query->bindValue(':ci', $ci);
         $query->bindValue(':pasaporte', $pasaporte);
