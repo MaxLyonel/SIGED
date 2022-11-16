8e4ecb1559d0587b3c2a459e7779b429064aba63
dancastillo <dan.c4stillo@gmail.com>
2022-11-16 13:17:52 -0400

    Validacion reseteos alternativa

--- src/Sie/AppWebBundle/Controller/BachillerExcelenciaAlternativaController.php
+++ src/Sie/AppWebBundle/Controller/BachillerExcelenciaAlternativaController.php
@@ -169,9 +169,9 @@ class BachillerExcelenciaAlternativaController extends Controller {
 
             $registrado = $query->getResult();
 
-            if(count($registrado) > 1) {
+            /*if(count($registrado) > 1) {
                 return $this->redirect($this->generateUrl('principal_web'));
-            }
+            }*/
 
             /*
             * Verificar si la UE cuenta con aprendizajes especializados
