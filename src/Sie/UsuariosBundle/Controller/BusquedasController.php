9eaca30a4dd70f925d0d923331113b3ceb374c7f
Iver Guaygua <iverosvaldo@gmail.com>
2023-04-27 18:46:47 -0400

    modificacion a susuariao -usuario

--- src/Sie/UsuariosBundle/Controller/BusquedasController.php
+++ src/Sie/UsuariosBundle/Controller/BusquedasController.php
@@ -66,7 +66,8 @@ class BusquedasController extends Controller
     
     public function formcarnetAction() {
 
-        if(!($this->session->get('roluser') == 8)){
+        $us = array(8,31);
+        if(!(in_array($this->session->get('roluser'), $us))){
             if(!in_array($this->session->get('userName'), $this->arrUserAllow)  ){
                 return $this->redirectToRoute('sie_usuarios_homepage');
             }
